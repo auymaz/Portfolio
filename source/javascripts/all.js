a8ac4b63e66ba15dd5cab848c2e5416973f0d3b9
Abdurrahman uymaz <appie_u_@hotmail.com>
2016-06-14 19:03:53 +0200

    changed some things

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,9 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
